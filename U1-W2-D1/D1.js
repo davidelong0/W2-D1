85f32d4d1d13063769c28a53c8291495e29372db
davidelong0 <davidelong0@hotmail.com>
2025-02-10 15:27:14 +0100

    AGGIORNATO-DESTINAZIONE

---
+++ U1-W2-D1/D1.js
@@ -0,0 +1,73 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per visualizzare l'output, lancia il file HTML a cui è collegato e apri la console dagli strumenti di sviluppo del browser. 
+- Utilizza dei console.log() per testare le tue variabili e/o i risultati delle espressioni che stai creando.
+*/
+
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+/*I principlai datatype in JavaScript si dividono in:
+string = Tutto il testo, le parole che andiamo a racchiudere tra le "virgolette" o gli 'accenti'.
+number = tutti inumeri interi o decimali .
+boolean = i valori logici che possono essere o true o false .
+undefined = sarà una varibiale senza un valore assegnato, ci potrà capitare di vederla quando commettiamo un errore ad esempio
+null = indicherà l'assenza di valore ma in questo caso non si tratterà di un valore non assegnato ma di un valore esplicitamente vuoto, a cui io ho scelto di dare zero contenuto.
+*/
+/* ESERCIZIO 2
+ Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let myName = "Davide";
+console.log(myName);
+/* ESERCIZIO 3
+ Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let firstNumber = 12;
+let secondNumber = 20;
+let addiction = firstNumber + secondNumber;
+console.log(addiction);
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let x = 12;
+/* ESERCIZIO 5
+  Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
+  Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+/* INCLUDO IN UN COMMENTO PER FAR SI CHE CONTINUI A FUNZIONARE
+let myName = "Longo";
+console.log(myName);
+*/
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let subtraction = 12 - 4;
+console.log(subtraction);
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let name1 = "john";
+let name2 = "John";
+console.log(name1 === name2);
+let lowercase = (name1 = name2);
+console.log(name1 === name2);
